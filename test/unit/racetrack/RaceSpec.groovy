4ba42534c8fde6eeeca460cdf1242644c53979f1
Saager Mhatre <saager.mhatre@gmail.com>
2014-01-08 21:30:33 +0530

    Add test for race.startDate custom validator

--- test/unit/racetrack/RaceSpec.groovy
+++ test/unit/racetrack/RaceSpec.groovy
@@ -15,4 +15,17 @@ class RaceSpec extends Specification {
       expect:
       race.inMiles() == 3.107
     }
+
+    void "should not accept past start date"() {
+      def race = new Race(startDate: new Date() - 1)
+
+      expect:
+      !race.validate()
+      race.hasErrors()
+      race.errors.getFieldError('startDate').codes.contains('race.startDate.validator.invalid')
+
+      println "Errors: ${ race.errors ?: "no errors found" }"
+      println "\nBadField: ${ race.errors.getFieldError('startDate') ?: "startDate wasn't a bad field" }"
+      println "\nCode: ${ race.errors.getFieldError('startDate').codes.find { it == 'race.startDate.validator.invalid' } ?: "startDate wasn't a bad field" }"
+    }
 }
